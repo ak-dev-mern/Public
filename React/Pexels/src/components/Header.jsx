5a37b17bab5ad0efaa5d3b0332c625939224e8a4
Ajith Kumar <ak.dev.mern@gmail.com>
2025-02-09 12:16:20 +0800

    change header component styles

--- React/Pexels/src/components/Header.jsx
+++ React/Pexels/src/components/Header.jsx
@@ -5,20 +5,37 @@ const Header = () => {
     <>
       <nav className="main-navbar w-100">
         <ul className="d-flex justify-content-center align-items-center">
-          <li>
-            <NavLink to="/">Home</NavLink>
+          <li className="nav-item">
+            <NavLink to="/" className="nav-link" activeClassName="active">
+              Home
+            </NavLink>
           </li>
-          <li>
-            <NavLink to="/videos">Videos</NavLink>
+          <li className="nav-item">
+            <NavLink to="/videos" className="nav-link" activeClassName="active">
+              Videos
+            </NavLink>
           </li>
-          <li>
-            <NavLink to="/leaderboard">Leaderboard</NavLink>
+          <li className="nav-item">
+            <NavLink
+              to="/leaderboard"
+              className="nav-link"
+              activeClassName="active"
+            >
+              Leaderboard
+            </NavLink>
           </li>
-          <li>
-            <NavLink to="/challenges">Challenges</NavLink>
+          <li className="nav-item">
+            <NavLink
+              to="/challenges"
+              className="nav-link"
+              activeClassName="active"
+            >
+              Challenges
+            </NavLink>
           </li>
         </ul>
       </nav>
+
       <div className="sub-header d-flex justify-content-between align-items-center px-5 mt-5">
         <h4>Free Stock Photos</h4>
         <div>
