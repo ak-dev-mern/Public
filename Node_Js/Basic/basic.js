0bd599f7dd60a95f6cd053b34bef00494f91bb1b
Ajith Kumar <ak.dev.mern@gmail.com>
2025-02-19 22:43:06 +0800

    start node js

---
+++ Node_Js/Basic/basic.js
@@ -0,0 +1,17 @@
+// console.log("Ajith");
+// console.log(global);
+
+// const os = require("os");
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
+
+// const path = require("path");
+// console.log(path.parse(__filename));
+
+const { add, sub, mul, div } = require("./maths");
+
+console.log(add(2, 2));
+console.log(sub(2, 2));
+console.log(mul(2, 2));
+console.log(div(2, 2));
